e1aea1f5b80ba067d5d684e114b50ae9b17e3f4e
thebuilderekes <ekeopreberedugo1@gmail.com>
2024-01-20 14:01:40 +0100

    added pages

---
+++ js/animation.js
@@ -0,0 +1,5 @@
+ScrollReveal({ reset: true });
+ScrollReveal().reveal(".headline", { duration: 500 });
+ScrollReveal().reveal(".punchline", { delay: 600 });
+ScrollReveal().reveal(".tagline", { delay: 700 });
+ScrollReveal().reveal(".widget", { interval: 800 });
